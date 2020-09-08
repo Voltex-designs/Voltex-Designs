225b45bbaf11f7ccc26f7f88bd9507f5d888fe31
Hanif Adedotun <54331442+Hanif-adedotun@users.noreply.github.com>
2020-09-08 06:16:00 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,72 @@
 //Javascript for Voltex SPAnpm install -g express-generator
 //history.back()
 //history.forward() for going back and forward in website
-window.onscroll = function() {
 
-  // Get the header
-var header = document.getElementsByTagName("header");
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+// window.onscroll = function() {
+// console.log('Scrolling');
+//   // Get the header
+// var header = document.getElementById("header");
+// // Get the offset position of the navbar
+// var headeroffset = header.offsetTop;
 
-};
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+//   if (window.pageYOffset > headeroffset) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
 
+// };
 $(document).ready(function(){
-   $('#banner').fadeIn('slow');
-  
+   
+   logoFadeIn();
+   footer_text();
+
    $('h2').hover(function(){
-    $(this).addClass('bigger')
+    $(this).addClass('bigger');
    },function(){
-    $(this).removeClass('bigger')
+    $(this).removeClass('bigger');
    });
 
    $('.footImg').hover(function(){
-      $(this).addClass('ImgBigger')
+      $(this).addClass('ImgBigger');
      },function(){
-      $(this).removeClass('ImgBigger')
+      $(this).removeClass('ImgBigger');
      });
 
 
+   //To show Netlify content
+   $('#subscribe_form').submit(function(e){
+      e.preventDefault();
+
+      var $form = $(this);
+      $.post($form.attr('action'), $form.serialize()).then(function(){
+         alert('Subscribed Successfully');
+      });
+   });
 }); 
 
-   var date = new Date();
-   var concDate = date.getHours + ':' + date.getMinutes;
-   document.getElementsByTagName('span').innerHtml = concDate;
+function logoFadeIn(){
+   $('#head-logo')
+   .hide('slow')
+   .show('slow');
+    
+}
+
+$(function () {
+   $('[data-toggle="popover"]').popover();
+   });
+
+function footer_text(){
+   // Display the footer text with current year
+   var text_element = document.getElementById('footer-text');
+   var current_year = new Date().getFullYear();
+   console.log(current_year);
+   text_element.innerHTML = '&copy;' + ' Voltex Designs ' + current_year;
+   }
+
+
+
 
 
 
